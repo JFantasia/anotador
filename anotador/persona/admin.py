8c3c8314a73c6a0372b820f9fe9cb2c642423530
José Fantasia <jose.fantasia@geneos.com.ar>
2022-11-02 06:18:12 -0300

    Ajuste por error de prueba gráficos

--- anotador/persona/admin.py
+++ anotador/persona/admin.py
@@ -1,11 +1,11 @@
 import json
-from django.core.serializers.json import DjangoJSONEncoder
+# from django.core.serializers.json import DjangoJSONEncoder
 from django.contrib import admin
 
 # Register your models here.
 from import_export.admin import ExportActionMixin
 from persona.models import Grupo, Persona
-from django.core import serializers
+# from django.core import serializers
 
 
 admin.site.register(Grupo)
